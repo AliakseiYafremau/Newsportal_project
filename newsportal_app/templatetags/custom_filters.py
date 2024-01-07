673309d6059bc8c74254c8c63e722a2fa6c68da8
Aliaksei Yafremau <447654972g@gmail.com>
2024-01-07 11:17:58 +0100

    dobavil censor

--- newsportal_app/templatetags/custom_filters.py
+++ newsportal_app/templatetags/custom_filters.py
@@ -1,9 +1,13 @@
 from django import template
-
+from ..profanities import profanity_list
 
 register = template.Library()
 
 
 @register.filter()
-def currency(value):
-   return f'{value} Р'
+def censor(text):
+    for first in range(len(text)):
+        for second in range(1, len(text)+1):
+            if text[first:second + 1] in profanity_list:
+                text = text[:first + 1] + '*' * (second - first) + text[second + 1:]
+    return text
