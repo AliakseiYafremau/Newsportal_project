678897bbba7ae3b3a027b54bc0829e4ab5ea309c
Aliaksei Yafremau <447654972g@gmail.com>
2024-01-11 16:24:51 +0100

    upgrade filter

--- newsportal_app/filters.py
+++ newsportal_app/filters.py
@@ -6,19 +6,19 @@ from .models import Post
 
 
 class PostFilter(FilterSet):
+    date_of_creation = django_filters.DateTimeFilter(field_name='date_of_creation', widget=forms.DateTimeInput(attrs={'type': 'date'}), lookup_expr='date')
+    date_of_creation__gt = django_filters.DateTimeFilter(field_name='date_of_creation', widget=forms.DateTimeInput(attrs={'type': 'date'}), lookup_expr='date__gt')
+    date_of_creation__lt = django_filters.DateTimeFilter(field_name='date_of_creation', widget=forms.DateTimeInput(attrs={'type': 'date'}), lookup_expr='date__lt')
+
     class Meta:
         model = Post
         fields = {
             'text': ['icontains'],
             'rating': ['gt'],
-            'date_of_creation': [
-                'lt',
-                'gt',
-            ],
         }
 
 
 class PostSearchFilter(FilterSet):
     text = django_filters.CharFilter(lookup_expr='icontains')
     author__user__username = django_filters.CharFilter(lookup_expr='icontains')
-    date_of_creation = django_filters.DateTimeFilter(widget=forms.DateTimeInput(attrs={'type': 'date'}))
+    date_of_creation = django_filters.DateTimeFilter(widget=forms.DateTimeInput(attrs={'type': 'date'}), lookup_expr='date')
